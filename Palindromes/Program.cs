73cc99f0b26545c658f7612fa7c91334d7caa5e5
WaysideTester <dylanmurray211@outlook.com>
2019-03-09 10:49:22 -0600

    We got it checking strings for validity of palindromes

--- Palindromes/Program.cs
+++ Palindromes/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Palindromes
 {
@@ -6,7 +7,45 @@ namespace Palindromes
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            string test1 = "Stars";
+
+            string test2 = "O, a kak Uwakov lil vo kawu kakao!";
+
+            string test3 = "Some men interpret nine memos";
+
+            bool PalinTest(string testString)
+            {
+                var chars = testString.ToCharArray();
+                List<char> updatedChars = new List<char>();
+                foreach (char item in chars) {
+                    if (Char.IsLetter(item))
+                    {
+                        updatedChars.Add(item);
+                    }
+                }
+                List<char> newUpdatedChars = new List<char>();
+                newUpdatedChars.AddRange(updatedChars);
+                newUpdatedChars.Reverse();
+
+                bool finalOutput = true;
+                for (int i = 0; i < newUpdatedChars.Count; i++)
+                {
+
+                    if (Char.ToLower(newUpdatedChars[i]) != Char.ToLower(updatedChars[i]))
+                    {
+                        finalOutput = false;
+                        break;
+                    }
+                }
+
+                return finalOutput;
+            }
+            Console.WriteLine($"{test1} is {(PalinTest(test1) ? "" : "not ")}a palindrome");
+            Console.WriteLine($"{test2} is {(PalinTest(test2) ? "" : "not ")}a palindrome");
+            Console.WriteLine($"{test3} is {(PalinTest(test3) ? "" : "not ")}a palindrome");
+            Console.WriteLine(PalinTest(test3));
+            Console.ReadLine();
         }
     }
 }
